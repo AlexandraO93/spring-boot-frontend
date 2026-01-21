36ffafd7277309f4c05e68cee9b8bb387ceaa00f
AlexandraO93 <alexandra.olsson93@hotmail.com>
2026-01-21 10:44:54 +0100

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: true
+    },
+    preview: {
+        allowedHosts: [
+            "inquisitive-emmye-miljanaa-0c55bd32.koyeb.app"
+        ]
+    }
 })
